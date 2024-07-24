a0fa62d67d28b86135097df6c61a3a3dd57ae611
ndukachukz <ndukachukwuemeka57@gmail.com>
2024-07-24 15:06:26 +0100

    update: bottom bar

--- src/components/shared/bottom-tab-bar.tsx
+++ src/components/shared/bottom-tab-bar.tsx
@@ -1,6 +1,9 @@
 import { GPSIcon, HomeIcon, UserIcon, WalletIcon } from "@/assets/icons";
+import { useAuth } from "@/hooks/useAuth";
+import { LogInIcon } from "lucide-react";
 
 function BottomTabBar() {
+  const { user, signIn } = useAuth();
   return (
     <nav className="lg:hidden bg-white p-4 flex justify-between items-center fixed bottom-0 left-0 right-0 rounded-t-lg shadow-2xl shadow-top">
       <a href="/">
@@ -12,9 +15,8 @@ function BottomTabBar() {
       <a href="">
         <WalletIcon className="size-8" />
       </a>
-      <a href="">
-        <UserIcon className="size-8" />
-      </a>
+      {!user && <LogInIcon onClick={signIn} className="size-8" />}
+      {user && <UserIcon className="size-8" />}
     </nav>
   );
 }
