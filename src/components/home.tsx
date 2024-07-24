27e6a5ab53243e15d682e18575045f81431db224
ndukachukz <ndukachukwuemeka57@gmail.com>
2024-07-24 15:09:24 +0100

    update: show user name

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,10 +1,13 @@
 import CategoriesSelect from "./shared/categories-select";
 import { Input } from "./ui/input";
 import List from "./list";
+import { useAuth } from "@/hooks/useAuth";
 
 function Home() {
+  const { user } = useAuth();
   return (
     <section>
+      {user?.name}
       <div className=" mb-6 md:mb-16 flex flex-col-reverse md:flex-row gap-6 justify-between">
         <CategoriesSelect />
 
