72a7f6f34b39d66edbfc0992f71600b9dc83145b
ndukachukz <ndukachukwuemeka57@gmail.com>
2024-07-24 15:26:32 +0100

    feat: removed api mutations

--- src/hooks/useCall.ts
+++ src/hooks/useCall.ts
@@ -1,8 +1,8 @@
 import { useEffect, useRef, useState } from "react";
-import { api } from "../lib/services/api";
+// import { api } from "../lib/services/api";
 import { call, usePeerStore } from "@/store/peer-store";
 
-const CHARGE_RATE = 0.1; // $0.1 per second
+// const CHARGE_RATE = 0.1; // $0.1 per second
 
 export function useCall(currentUser: User | null) {
   const [activeCall, setActiveCall] = useState<Call | null>(null);
@@ -21,14 +21,14 @@ export function useCall(currentUser: User | null) {
   const startCall = async (receiverId: string) => {
     if (!currentUser) return;
 
+    /* 
     const newCall: Omit<Call, "id"> = {
       callerId: currentUser.id,
       receiverId,
       startTime: Date.now(),
-    };
+    }; */
 
-    const createdCall = await api.createCall(newCall);
-    setActiveCall(createdCall);
+    // const createdCall = await api.createCall(newCall);
 
     console.log("Calling... ", receiverId);
 
@@ -37,12 +37,12 @@ export function useCall(currentUser: User | null) {
 
   const endCall = async () => {
     if (!activeCall || !currentUser) return;
-
+    /* 
     const endTime = Date.now();
     const duration = (endTime - activeCall.startTime) / 1000; // in seconds
     const cost = duration * CHARGE_RATE;
 
-    const updatedCall: Call = {
+     const updatedCall: Call = {
       ...activeCall,
       endTime,
       duration,
@@ -62,7 +62,7 @@ export function useCall(currentUser: User | null) {
       await api.updateUser({ ...caller, balance: caller.balance - cost });
       await api.updateUser({ ...receiver, balance: receiver.balance + cost });
     }
-
+ */
     setActiveCall(null);
   };
 
